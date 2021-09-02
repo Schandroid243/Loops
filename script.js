0a6924f09afc87510bd6fb9a029c1763c0ab83f7
Schandroid243 <schadrackngunza@gmail.com>
2021-09-02 17:16:36 +0100

    Learning the while loop

--- script.js
+++ script.js
@@ -6,10 +6,28 @@ var todos = [
     "eat healthy"
 ];
 var todosLength =todos.length;
+
+/* The For Loop */
+
 for(var i = 0; i < todos.length; i++) {
     todos[i] = todos[i] + "!";
 }
 
 for(var i = 0; i < todosLength; i++) {
     todos.pop();
+}
+
+/* The While Loop */
+
+var counterOne =  0;
+while (counterOne < 10) {
+    console.log(counterOne);
+    counterOne++;
+}
+
+//Here we're doing a countdown
+var counterTwo =  10;
+while (counterTwo > 0) {
+    console.log(counterOne);
+    counterTwo--;
 }
